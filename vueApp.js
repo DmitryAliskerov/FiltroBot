4a7682ed7ea29d721c8826387bf84daf1e3a292a
Dmitry Aliskerov <dmitry.aliskerov@gmail.com>
2024-09-07 16:34:38 +0400

    Bug fix

--- vueApp.js
+++ vueApp.js
@@ -7,8 +7,6 @@ const vm = new Vue({
 	},
 	methods: {
 		save: function(e) {
-			e.preventDefault()
-
 			diff = this.results.filter(x => this.origin.filter(y => y[0] == x[0] && y[3] != x[3]).length != 0)
 
 			Telegram.WebApp.sendData(
@@ -29,6 +27,7 @@ const vm = new Vue({
 			.get(base_url + user_id)
 			.then(response => {
 				this.results = response.data
+				this.origin = this.results.map(a => Object.assign({}, a))
 				this.loading = false
 			})
 			.catch(error => {
