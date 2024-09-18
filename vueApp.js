15912f5987564d599f0b4d2bda7b0cbd57c801fb
Dmitry Aliskerov <dmitry.aliskerov@gmail.com>
2024-09-18 13:25:00 +0400

    Add sorting

--- vueApp.js
+++ vueApp.js
@@ -4,7 +4,15 @@ const vm = new Vue({
 		search: '',
 		loading: true,
 		status: "Loading...",
-		results: []
+		results: [],
+		sortOptions: [
+			{id: 0, val: "Время"},
+			{id: 1, val: "Канал -> Время"},
+			{id: 2, val: "Канал -> Тема -> Время"},
+			{id: 3, val: "Тема -> Время"},
+			{id: 4, val: "Тема -> Канал -> Время"},
+		],
+		selectedSortOption: 0
 	},
 	methods: {
 		save: function(e) {
@@ -14,7 +22,8 @@ const vm = new Vue({
 				JSON.stringify({
 					"user_id": user_id,
 					"chat_ids_to_delete": diff.filter(x => !x[3]).map(x => x[0]),
-					"chat_ids_to_insert": diff.filter(x => x[3]).map(x => x[0])
+					"chat_ids_to_insert": diff.filter(x => x[3]).map(x => x[0]),
+					"sort_option": this.selectedSortOption
 			}))
 		}
 	},
@@ -26,11 +35,18 @@ const vm = new Vue({
 		}
 	},
 	mounted() {
+/*
+		this.results = [[-1001101170442, 'rian_ru', 'РИА Новости 1', true], [-1001101170443, 'rian_ru', 'РИА Новости 2', true]]
+		this.origin = this.results.map(a => Object.assign({}, a))
+		this.loading = false
+		return
+*/
 		axios
 			.get(base_url + user_id)
 			.then(response => {
-				this.results = response.data
+				this.results = response.data.results
 				this.origin = this.results.map(a => Object.assign({}, a))
+				this.selectedSortOption = response.data.sortOption
 				this.loading = false
 			})
 			.catch(error => {
